79e9c1e8e3d6c709e352579c05a4f571b158b19b
Maria Victoria Ching <70054433+mvtc01@users.noreply.github.com>
2021-03-20 22:47:09 +0800

    Update lab2_ex2.c

--- lab2_ex2.c
+++ lab2_ex2.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+
+
 int main(void)
 {
-    printf("Hello World!\n");
+	FILE *fp;
+	int len;
+	char phrase[20];
+	printf("Enter input string: ");
+	scanf("%[^\n]s",phrase);
+	len = strlen(phrase);
+	fp = fopen("output.txt","w");
+	fputs(phrase,fp);
+	puts(phrase);
+	fclose(fp);
 
+for(int i = 0; i < len-1; i++){     
+        int i3, end;
+		end = phrase[len-1];    
+        
+        for(i3 = len - 1; i3 > 0; i3--){    
+            phrase[i3] = phrase[i3-1];   
+        }    
+  									    
+        phrase[i3] = end;			    
+        puts(phrase);
+        fp = fopen("output.txt","a");
+        fputc('\n',fp);
+        fputs(phrase,fp);
+        fclose(fp);
+    }
+    
     return 0;
 }
+
