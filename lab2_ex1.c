fdad76d3a8e7079876299a2b3f599f22201708a7
Maria Victoria Ching <70054433+mvtc01@users.noreply.github.com>
2021-03-20 18:31:31 +0800

    Update lab2_ex1.c

--- lab2_ex1.c
+++ lab2_ex1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(void)
 {
-    printf("Hello World!\n");
+	int len;
+	char phrase[20];
+	printf("Enter input string: ");
+	gets(phrase);
+	len = strlen(phrase);
+	puts(phrase);
 
+for(int i = 0; i < len-1; i++){     //loops (length of sphrase)-2 number of times, shifts array elements by 1 in every loop
+        int i3;    
+        
+        for(i3 = len - 1; i3 > 0; i3--){    
+            phrase[i3] = phrase[i3-1];   
+        }    
+  									    
+        phrase[i3] = ' ';			    
+        puts(phrase);
+    }
     return 0;
 }
